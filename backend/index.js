e18d81f42fdcb5bbc6ae1df6cf587d78c038c850
Aromal K Prakash <aromalkprakashofficial@gmail.com>
2024-05-06 20:56:46 +0530

    created API for add and remove products(collections) from db

--- backend/index.js
+++ backend/index.js
@@ -77,7 +77,19 @@ const Product = mongoose.model("Product",{
 })
 
 app.post('/addproduct',async (req,res)=>{
+    let products = await Product.find({});
+    let id;
+    if(product.length>0)
+        {
+            let last_product_array = products.slice(-1);
+            let last_product = last_product_array[0];
+            id = last_product.id+1;
+        }
+        else{
+            id=1
+        }
     const product = new Product({
+        id:id,
         id:req.body.id,
         name:req.body.name,
         image:req.body.image,
@@ -94,6 +106,17 @@ app.post('/addproduct',async (req,res)=>{
     })
 })
 
+
+// Creating API For deleting Products
+
+app.post('/removeproduct',async (req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("Removed");
+    res.json({
+        success:true,
+        name:req.body.name
+    })
+})
 
 
 app.listen(port, () => {
