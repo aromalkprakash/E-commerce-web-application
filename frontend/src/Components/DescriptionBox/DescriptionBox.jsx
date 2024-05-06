0b2bf252da5d2c0772b5e5ad08353e35babacd75
Aromal K Prakash <aromalkprakashofficial@gmail.com>
2024-05-06 13:03:13 +0530

    cart designed

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews(122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p> E-commerce, short for electronic commerce, is the buying and selling of goods and services 
+            over the internet. It encompasses a wide range of online business activities for products 
+            and services. E-commerce enables consumers to electronically exchange goods and 
+            services with no barriers of time or distance. It has expanded rapidly over the years and is 
+            predicted to continue at this rate, or even accelerate. It can involve transactions between 
+            businesses, consumers, governments, or other entities, and operates through 
+            various platforms such as online marketplaces, retail websites, and mobile apps.</p>
+            <p>
+                
+            </p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
