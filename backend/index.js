cc67493390a44f5617cfcb0dfacd5ea458391c83
Aromal K Prakash <aromalkprakashofficial@gmail.com>
2024-05-09 15:27:59 +0530

    created endpoint and fetched data to popular in women

--- backend/index.js
+++ backend/index.js
@@ -124,10 +124,19 @@ app.post('/login',async (req,res)=>{
 // Creating endpoint for newcollections data
 app.get('/newcollections',async (req,res)=>{
     let products = await Product.find({});
-    let newcollection = products.slice(1).slice(-8);
+    let newcollection = products.slice(1).slice(-8);  // product db de copy ondakitt kanikkum...
     console.log("NewCollection Fetched");
     res.send(newcollection);
 })
+
+// creating endpoint for popular in women section
+app.get('/popularinwomen',async (req,res)=>{
+    let products = await Product.find({category:"women"});
+    let popular_in_women = products.slice(0,4);
+    console.log("Popular in women fetch");
+    res.send(popular_in_women)
+})
+
 
 
 // Schema for Creating Products
